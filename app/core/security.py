0705234a63249da55b2a1c384712d5d217cbdb15
Elena Dobruk <elena.dobruk@yandex.ru>
2025-01-28 17:36:39 +0300

    add jwt token settings

---
+++ app/core/security.py
@@ -0,0 +1,39 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from app.core.config import get_settings
+from datetime import datetime, timedelta
+import jwt
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login')
+
+settings = get_settings()
+
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode, key=settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+
+def decode_token(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+def get_user_from_token(payload: dict = Depends(decode_token)):
+    return payload.get('sub')
