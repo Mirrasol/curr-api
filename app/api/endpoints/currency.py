f2f02257872378e18912fa452d697a6b14c222f7
Elena Dobruk <elena.dobruk@yandex.ru>
2025-01-28 21:15:29 +0300

    add authentication requirements to currency endpoints

--- app/api/endpoints/currency.py
+++ app/api/endpoints/currency.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.api.schemas.currency import Currency
+from app.core.security import get_user_from_token
 from app.utils.external_api import get_currencies_list, get_current_exchange_rates
 
 currency_router = APIRouter(
@@ -9,12 +10,12 @@ currency_router = APIRouter(
 
 
 @currency_router.get('/list')
-def show_currencies_list():
+def show_currencies_list(user: str = Depends(get_user_from_token)):
     currency_list = get_currencies_list()
     return currency_list
 
 
 @currency_router.post('/exchange')
-def exchange_currency(currencies: Currency):
+def exchange_currency(currencies: Currency, user: str = Depends(get_user_from_token)):
     result = get_current_exchange_rates(currencies)
     return result
